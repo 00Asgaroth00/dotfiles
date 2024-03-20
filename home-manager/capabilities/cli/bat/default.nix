5b899bcb19228ab9577a783cfde216283cc972cc
Asgaroth <00asgaroth00@gmail.com>
2024-03-20 19:04:52 +0000

    wsl (wip)

--- home-manager/capabilities/cli/bat/default.nix
+++ home-manager/capabilities/cli/bat/default.nix
@@ -1,12 +1,24 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   # enhanced cat
 
   programs.bat = {
     enable = true;
     config = {
-      theme = "base16";
+      theme = "tokyonight_night";
     };
   };
+
+  xdg.configFile."bat/themes/tokyonight_night.tmTheme".source =
+    pkgs.fetchFromGitHub
+    {
+      owner = "folke";
+      repo = "tokyonight.nvim";
+
+      rev = "v3.0.1";
+      sha256 = "sha256-QKqCsPxUyTur/zOUZdiT1cOMSotmTsnOl/3Sn2/NlUI";
+
+    }
+    + "/extras/sublime/tokyonight_night.tmTheme"; 
 
 }
